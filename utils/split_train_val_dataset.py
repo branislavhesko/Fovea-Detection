8b1fbf36f426e0e46a7ab50a15d0311920522930
branislav.hesko <branislav.hesko@protonmail.ch>
2020-06-06 22:38:28 +0200

    update

--- utils/split_train_val_dataset.py
+++ utils/split_train_val_dataset.py
@@ -1,9 +1,20 @@
+import cv2
 import os
 import glob
 import shutil
 from random import shuffle
 
-ratio = 0.85
+
+def process_image(src, destination, shape):
+    cv2.imwrite(destination, cv2.resize(cv2.imread(src, cv2.IMREAD_COLOR), shape))
+
+
+def process_mask(src, destination, shape):
+    cv2.imwrite(destination, cv2.resize(cv2.imread(src, cv2.IMREAD_GRAYSCALE), shape, interpolation=cv2.INTER_NEAREST))
+
+
+SHAPE = (1024, 1024)
+RATIO = 0.85
 images = sorted(glob.glob("./data/images/*.jpg"))
 masks = sorted(glob.glob("./data/GT_disc_cup/*.bmp"))
 
@@ -20,11 +31,12 @@ assert len(images) == len(masks)
 
 combined = list(zip(images, masks))
 shuffle(combined)
-to_train_number = int(len(combined) * ratio)
+to_train_number = int(len(combined) * RATIO)
 for idx, (image, mask) in enumerate(combined):
+    print("Processing image: {}, mask: {}".format(image, mask))
     if idx < to_train_number:
-        shutil.copy2(image, os.path.join("./data/train/images", os.path.split(image)[1]))
-        shutil.copy2(mask, os.path.join("./data/train/masks", os.path.split(mask)[1]))
+        process_image(image, os.path.join("./data/train/images", os.path.split(image)[1]), SHAPE)
+        process_mask(mask, os.path.join("./data/train/masks", os.path.split(mask)[1]), SHAPE)
     else:
-        shutil.copy2(image, os.path.join("./data/validate/images", os.path.split(image)[1]))
-        shutil.copy2(mask, os.path.join("./data/validate/masks", os.path.split(mask)[1]))
+        process_image(image, os.path.join("./data/validate/images", os.path.split(image)[1]), SHAPE)
+        process_mask(mask, os.path.join("./data/validate/masks", os.path.split(mask)[1]), SHAPE)
