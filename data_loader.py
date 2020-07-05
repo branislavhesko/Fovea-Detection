0a92832f6dfbc0ebcca9c9e9fd0037659eac63e6
Branislav Hesko <branislav.hesko@protonmail.ch>
2020-07-05 22:53:38 +0200

    addeddataloader staff

--- data_loader.py
+++ data_loader.py
@@ -14,16 +14,23 @@ class FoveaLoader(Dataset):
     def __init__(self, config):
         super().__init__()
         self._config = config
-        self._fovea_gt = pd.DataFrame()
+        self._fovea_gt = {}
         self._images = []
+        self._load_annotations(config.path)
+        self._load_images(config.path)
 
     def _load_annotations(self, path):
-        xlss = glob.glob(osp.join(path, "*.xls"))
+        xlss = glob.glob(osp.join(path, "fovea", "*.xlsx"))
         for xls in xlss:
             data = pd.read_excel(xls)
-            self._fovea_gt["ImgName"] = data["ImgName"]
-            self._fovea_gt["Fovea_X"] = data["Fovea_X"]
-            self._fovea_gt["Fovea_Y"] = data["Fovea_Y"]
+            if "ImgName" in self._fovea_gt:
+                self._fovea_gt["ImgName"] = pd.concat([data["ImgName"], self._fovea_gt["ImgName"]], ignore_index=True)
+                self._fovea_gt["Fovea_X"] = pd.concat([data["Fovea_X"], self._fovea_gt["Fovea_X"]], ignore_index=True)
+                self._fovea_gt["Fovea_Y"] = pd.concat([data["Fovea_Y"], self._fovea_gt["Fovea_Y"]], ignore_index=True)
+            else:
+                self._fovea_gt["ImgName"] = data["ImgName"]
+                self._fovea_gt["Fovea_X"] = data["Fovea_X"]
+                self._fovea_gt["Fovea_Y"] = data["Fovea_Y"]
 
     def _load_images(self, path):
         self._images = glob.glob(osp.join(path, "images", "*.jpg"))
@@ -33,13 +40,24 @@ class FoveaLoader(Dataset):
         return len(self._images)
 
     def __getitem__(self, item):
-        img_name, fovea_x, fovea_y = self._fovea_gt.iloc[item, :]
+        img_name, fovea_x, fovea_y = self._fovea_gt["ImgName"].iloc[item], \
+                                     int(self._fovea_gt["Fovea_X"].iloc[item]), \
+                                     int(self._fovea_gt["Fovea_Y"].iloc[item])
         img = cv2.cvtColor(cv2.imread(list(filter(
             lambda x: img_name in x, self._images))[0], cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB) / 255.
-        fovea_gt = self._prepare_mask(fovea_x, fovea_y, img.shape[:2])
+        fovea_gt = self.make_mask(img.shape[:2], 21, (fovea_y, fovea_x))
+        return img, fovea_gt
 
-    def _prepare_mask(self, fovea_x, fovea_y, shape):
-        return np.zeros_like(shape)
+    @staticmethod
+    def make_mask(mask_size, kernel_size, point):
+        mask = np.zeros((mask_size[0] + kernel_size, mask_size[1] + kernel_size))
+        gaussian_filter = cv2.getGaussianKernel(kernel_size, sigma=np.sqrt(kernel_size))
+        gaussian_kernel = gaussian_filter @ gaussian_filter.T
+        gaussian_kernel /= np.amax(gaussian_kernel)
+        mask[point[0]: point[0] + kernel_size, point[1]: point[1] + kernel_size] = gaussian_kernel
+        mask = mask[(kernel_size - 1) // 2: -1 - (kernel_size - 1) // 2,
+                    (kernel_size - 1) // 2: -1 - (kernel_size - 1) // 2]
+        return mask.astype(np.float32)
 
 
 def get_data_loader(config):
@@ -47,3 +65,10 @@ def get_data_loader(config):
     # TODO: finish
     data_loader = DataLoader(dataset)
     return data_loader
+
+
+if __name__ == "__main__":
+    from collections import namedtuple
+    X = namedtuple("X", ["path"])
+    dataset = FoveaLoader(X(path="./data/"))
+    print(dataset[0])
